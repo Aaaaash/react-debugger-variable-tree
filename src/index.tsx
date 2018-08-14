552e5411bd2a285a7e580e7e01e0e6a820b34289
sakuraash <841105127@qq.com>
2018-08-14 18:17:25 +0800

    initialize storybook

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,11 @@
 import React, { PureComponent } from 'react';
 
 class VariableTree extends PureComponent {
-
+  render() {
+    return (
+      <div>hello world</div>
+    );
+  }
 }
 
 export default VariableTree;
---
+++ stories/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import VariableTree from '../src';
+
+storiesOf('VariableTree', module).add('to Storybook', () => <VariableTree />);
